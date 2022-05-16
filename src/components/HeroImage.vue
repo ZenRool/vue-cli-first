d9558c80d97877dcfcded0ea6838d372ab918500
Zenrool <lorenzopapperini@hotmail.it>
2022-05-16 21:49:01 +0200

    Aggiunto componente img come jumbotron, importato anche esso nel App.vue

---
+++ src/components/HeroImage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="jumbo">
+    <img :src="require(`../assets/img/${srcImage}`)" alt="Banner" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroImage",
+  data: function () {
+    return {
+      srcImage: "jumbo.png",
+    };
+  },
+};
+</script>
+<style scoped>
+.jumbo {
+  width: 100%;
+  height: auto;
+}
+img {
+  width: 100%;
+  object-fit: contain;
+}
+</style>
